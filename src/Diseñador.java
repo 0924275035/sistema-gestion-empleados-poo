24ee425cc0b996cf2a42f6335b2a22512780f47c
Manuela Maldonado <maldonadozamoramanuela@gmail.com>
2025-11-05 21:09:16 -0500

    Estructura completa del proyecto - Manuela Maldonado

--- src/Diseñador.java
+++ src/Diseñador.java
@@ -1,3 +1,20 @@
-public class Diseñador {
+public class Diseñador extends Empleado {
+    
+    private String herramientaPrincipal;
+
+    public Diseñador(String nombre, int id, double salario, String herramienta) {
+        super(nombre, id, salario);
+        this.herramientaPrincipal = herramienta;
+    }
+
+    public void crearPrototipo() {
+        System.out.println(nombre + " está creando un prototipo en " + herramientaPrincipal + ".");
+    }
+
+    @Override
+    public void trabajar() {
+        System.out.println(nombre + " está diseñando interfaces.");
+    }
+}
     
 }
