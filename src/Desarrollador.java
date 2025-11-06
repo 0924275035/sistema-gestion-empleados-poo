be847370095b1124611665243756a6b5affceb97
Nicole Alejandra Apolinario Zapata <nicole.apolinariozapata8030@upse.edu.ec>
2025-11-05 22:40:04 -0500

    Agrego clase Gerente -Nicole Apolinario

--- src/Desarrollador.java
+++ src/Desarrollador.java
@@ -1,3 +1,17 @@
-public class Desarrollador {
-    
+public class Desarrollador extends Empleado {
+    private String lenguajePrincipal;
+
+    public Desarrollador(String nombre, int id, double salario, String lenguaje) {
+        super(nombre, id, salario);
+        this.lenguajePrincipal = lenguaje;
+    }
+
+    public void escribirCodigo() {
+        System.out.println(getNombre() + " está escribiendo código en " + lenguajePrincipal);
+    }
+
+    @Override
+    public void trabajar() {
+        System.out.println(getNombre() + " está desarrollando software.");
+    }
 }
