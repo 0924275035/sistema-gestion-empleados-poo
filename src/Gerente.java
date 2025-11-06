fc3de6b7ba3d20dc2564000a7bd7076166aff149
Nicole <choeznicole9@gmail.com>
2025-11-05 21:44:15 -0500

    Agrego clase Gerente - Nicole Choez

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,3 +1,18 @@
-public class Gerente {
-    
+public class Gerente extends Empleado {
+
+    private int numSubordinados;
+
+    public Gerente(String nombre, int id, double salario, int numSubordinados) {
+        super(nombre, id, salario);
+        this.numSubordinados = numSubordinados;
+    }
+
+    public void coordinarEquipo() {
+        System.out.println(nombre + " está coordinando un equipo de " + numSubordinados + " personas.");
+    }
+
+    @Override
+    public void trabajar() {
+        System.out.println(nombre + " está gestionando el proyecto.");
+    }
 }
